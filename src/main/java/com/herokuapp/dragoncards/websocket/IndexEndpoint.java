0e43ba73810b30da32a5ef334720b7d44d46ee36
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2014-04-22 03:49:54 -0700

    add encoders to IndexEndpoint

--- src/main/java/com/herokuapp/dragoncards/websocket/IndexEndpoint.java
+++ src/main/java/com/herokuapp/dragoncards/websocket/IndexEndpoint.java
@@ -13,11 +13,34 @@ import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 import com.herokuapp.dragoncards.decoders.MessageDecoder;
+import com.herokuapp.dragoncards.encoders.CreatePlayerMessageEncoder;
+import com.herokuapp.dragoncards.encoders.DrawMessageEncoder;
+import com.herokuapp.dragoncards.encoders.DuelRequestedMessageEncoder;
+import com.herokuapp.dragoncards.encoders.MovePlayerToLobbyMessageEncoder;
+import com.herokuapp.dragoncards.encoders.MovePlayerToRoomMessageEncoder;
+import com.herokuapp.dragoncards.encoders.OpponentDrawMessageEncoder;
+import com.herokuapp.dragoncards.encoders.OpponentPilferMessageEncoder;
+import com.herokuapp.dragoncards.encoders.OpponentSummonMessageEncoder;
+import com.herokuapp.dragoncards.encoders.QueryBattleActionsMessageEncoder;
+import com.herokuapp.dragoncards.encoders.QueryPlayerNameMessageEncoder;
+import com.herokuapp.dragoncards.encoders.QueryPreliminaryActionMessageEncoder;
+import com.herokuapp.dragoncards.encoders.UpdateLobbyMessageEncoder;
 
 @ServerEndpoint(
     value = "/index",
     encoders = {
-
+        CreatePlayerMessageEncoder.class,
+        DrawMessageEncoder.class,
+        DuelRequestedMessageEncoder.class,
+        MovePlayerToLobbyMessageEncoder.class,
+        MovePlayerToRoomMessageEncoder.class,
+        OpponentDrawMessageEncoder.class,
+        OpponentPilferMessageEncoder.class,
+        OpponentSummonMessageEncoder.class,
+        QueryBattleActionsMessageEncoder.class,
+        QueryPlayerNameMessageEncoder.class,
+        QueryPreliminaryActionMessageEncoder.class,
+        UpdateLobbyMessageEncoder.class
     },
     decoders = {
         MessageDecoder.class
